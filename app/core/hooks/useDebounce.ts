9deab6bc0c7e046f52c3497e78df9894d1e7070d
CamiloSinningUN <camilosinning.cs@gmail.com>
2023-05-26 00:07:58 -0500

    undo fix debounce

--- app/core/hooks/useDebounce.ts
+++ app/core/hooks/useDebounce.ts
@@ -2,24 +2,15 @@ import { useRef, useEffect, useCallback } from 'react';
 
 const useDebounce = <T extends (...args: any[]) => any>(fn: T, delay: number): T => {
 	const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-	const firstCall = useRef<boolean>(true);
 
 	const debouncedFn = useCallback((...args: Parameters<T>) => {
-		if (firstCall.current) {
-			fn(...args);
-			firstCall.current = false;
-			timeoutRef.current = setTimeout(() => {
-				firstCall.current = true;
-			}, delay);
-		} else {
-			if (timeoutRef.current) {
-				clearTimeout(timeoutRef.current);
-			}
-			timeoutRef.current = setTimeout(() => {
-				fn(...args);
-				firstCall.current = true;
-			}, delay);
+		if (timeoutRef.current) {
+			clearTimeout(timeoutRef.current);
 		}
+
+		timeoutRef.current = setTimeout(() => {
+			fn(...args);
+		}, delay);
 	}, [fn, delay]) as T;
 
 	useEffect(() => {
@@ -34,5 +25,4 @@ const useDebounce = <T extends (...args: any[]) => any>(fn: T, delay: number): T
 };
 
 export default useDebounce;
-
 
